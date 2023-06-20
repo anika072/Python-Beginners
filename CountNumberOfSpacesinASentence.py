26f7016aa415fe91b770a82f43e5e57f0b0c7721
Anika Tabassum Nafisa <108858332+anika072@users.noreply.github.com>
2023-06-20 21:24:13 +0600

    Update CountNumberOfSpacesinASentence.py

--- CountNumberOfSpacesinASentence.py
+++ CountNumberOfSpacesinASentence.py
@@ -1,4 +1,4 @@
-Count number of spaces in a sentence-->
+##Count number of spaces in a sentence-->
 some_string = 'the slow solid squid swam sumptuously through the slimy swamp'
 num_space=[x for x in some_string if x==' ']
 print(len(num_space))
